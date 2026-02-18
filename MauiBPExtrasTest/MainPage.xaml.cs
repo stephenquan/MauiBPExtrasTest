1025cac8d588661aa4c950e5d177e725e593b1dd
Stephen Quan <squan@esri.com>
2026-02-18 13:34:18 +1100

    Revert example to make it look closer to the .NET MAUI starter app

--- MauiBPExtrasTest/MainPage.xaml.cs
+++ MauiBPExtrasTest/MainPage.xaml.cs
@@ -5,27 +5,38 @@ using BPExtrasAttribute = SQuan.Helpers.Labs.BPExtrasAttribute;
 
 namespace MauiBPExtrasTest;
 
+#pragma warning disable IDE0011
+
 public partial class MainPage : ContentPage
 {
 	[BindableProperty(PropertyChangedMethodName = nameof(OnCountChanged))]
 	[BPExtras]
 	public partial int Count { get; set; } = 0;
+
 	partial void OnCountChanged(int value)
 	{
 		System.Diagnostics.Trace.WriteLine($"Count has changed to {value}");
 	}
+
 	partial void OnCountChanged(int oldValue, int newValue)
 	{
 		System.Diagnostics.Trace.WriteLine($"Count has changed from {oldValue} to {newValue}");
 	}
+
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
 	void OnCounterClicked(object? sender, EventArgs e)
 	{
 		Count++;
-		CounterBtn.Text = $"Clicked {Count} times";
+
+		if (Count == 1)
+			CounterBtn.Text = $"Clicked {Count} times";
+		else
+			CounterBtn.Text = $"Clicked {Count} times";
+
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 }
